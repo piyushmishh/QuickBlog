623d72dc67fd8815111cc058ac3d700d1eade28d
piyushmishh <piyush.mishra2107@gmail.com>
2025-10-05 17:27:32 +0530

    Till BlogList

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from 'react'
 import { blogCategories } from '../assets/assets'
-import { motion } from "motion/react"
+import { motion } from "framer-motion" 
 
 const BlogList = () => {
   
-  const[menu,setMenu]=useState("All")
+  const [menu, setMenu] = useState("All")
+  
   return (
-    <div>
-        <div className='flex justify-center gap-4 sm:gap-8 my-10
-        relative'>
-           {blogCategories.map((item)=>(
-            <div key={item} className='relative'>
-                <button onClick={()=>setMenu(item)}
-                 className={`cursor-pointer text-gray-500 ${menu ===item && 
-                    'text-white px-4 pt-0.5'}`}>
-                    {item}
-                    {menu === item && (
-                        <div className='absolute left-0 right-0
-                    top-0 h-7 -z-1 bg-primary rounded-full'></div>  /*only adding background color for the selected item */
-                    )}
-                </button>
-            </div>
-           ))}
-        </div>
-        <div>
-            {/* ---blog cards---  */}
-        </div>
+    <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
+      
+      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+        
+        {blogCategories.map((item) => (
+          <div key={item} className='relative'>
+            <button 
+              onClick={() => setMenu(item)}
+              // Added 'relative' to the button for absolute positioning of the motion.div
+              className={`relative cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}
+            >
+              {item}
+              
+              {/* Animated Background Underline (renders only on selected item) */}
+              {menu === item && (
+                <motion.div 
+                  layoutId='underline'
+                  transition={{ type: 'spring', stiffness: 500, damping: 30 }} 
+                  className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'
+                /> 
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+      
+      <div>
+        {/* ---blog cards---  */}
+      </div>
     </div>
   )
 }
