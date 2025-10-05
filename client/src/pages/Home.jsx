33e7317e19b6f6dd5e08b1bd5f6bf0af7477b855
piyushmishh <piyush.mishra2107@gmail.com>
2025-10-05 16:58:40 +0530

    Till blogList

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Header from '../components/Header'
+import BlogList from '../components/BlogList'
 
 const Home = () => {
   return (
     <>
         <NavBar/>
         <Header/>
+        <BlogList/>
     </>
   )
 }
